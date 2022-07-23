20a6344a84a815863cfdfe595095aaea351083da
Nata <Beloborodova.n.s@bk.ru>
2022-07-23 23:50:49 +0300

    dz

---
+++ dz/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 25: Напишите цикл, который принимает
+//на вход два числа (A и B) и возводит число A в 
+// натуральную степень B.
+
+double A, B;
+double result = 1;
+Console.WriteLine("Введите число A: ");
+A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите степень, в которую нужно возвести число: ");
+B = Convert.ToDouble(Console.ReadLine());
+while (B > 0)
+{ 
+   result = result * A; 
+   B --;
+} 
+{
+    Console.WriteLine (result);
+}
+
